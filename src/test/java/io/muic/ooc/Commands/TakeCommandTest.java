52fb971cc0a320d34d7b30f9fcebb2c71f0e72ff
karn806 <nattakarn.kl@gmail.com>
2017-02-04 00:52:51 +0700

    added one more assert

--- src/test/java/io/muic/ooc/Commands/TakeCommandTest.java
+++ src/test/java/io/muic/ooc/Commands/TakeCommandTest.java
@@ -1,5 +1,6 @@
 package io.muic.ooc.Commands;
 
+import io.muic.ooc.Items.Item;
 import io.muic.ooc.Items.Potion;
 import io.muic.ooc.Player;
 import org.junit.Test;
@@ -14,7 +15,12 @@ public class TakeCommandTest {
     public void apply() throws Exception {
         Potion potion = new Potion();
         Player player = new Player();
+        player.setBag(potion);
+
+        assertTrue(potion == player.getBag().get(0));
 
     }
+
+
 
 }
